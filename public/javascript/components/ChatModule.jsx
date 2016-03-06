7713a542a3802c685638da98da817b9269334220
yoshioka-s <yoshioka63@gmail.com>
2016-03-05 16:38:48 -0800

    install react

--- public/javascript/components/ChatModule.jsx
+++ public/javascript/components/ChatModule.jsx
@@ -1,22 +1,58 @@
 var React = require('react');
-var $ = require('jquery');
-var Promise = require('bluebird');
+// var $ = require('jquery');
+// var Promise = require('bluebird');
 var QB = require('quickblox');
 
 var ChatModule = React.createClass({
+  getInitialState: function () {
+    return {
+      isFormShown: false
+    };
+  },
+
+  /**
+  * open the chat form
+  */
+  toggleChat: function () {
+    if (this.state.isFormShown) {
+      this.setState({isFormShown: false});
+    } else {
+      this.setState({isFormShown: true});
+    }
+  },
+
+  /**
+  * send user message to server
+  */
+  submitMessage: function () {
+    var newMessage = this.state.newMessage;
+    // TODO: post request to the server
+    this.setState({newMessage: ''});
+  }
+
+
   render: function () {
+    var formToggleClass = this.state.isFormShown ? 'shown' : 'hidden';
+    var chatNowText = this.state.isFormShown ? 'Hide Chat' : 'Chat Now';
+
     return (
       <div className="quickblox-chat">
         <h4>Hello</h4>
-        <button type="button" className="btn" id="begin-chat">Chat Now</button>
-        <div className="chat-form">
+        <button className="btn" onClick={this.toggleChat} >{chatNowText}</button>
+        <div className={formToggleClass + ' chat-form'}>
           <div className="chat-display">
-
+            <div className="message customer">
+              customer message
+            </div>
+            <div className="message admin">
+              admin message
+            </div>
+            <br className="clear"></br>
           </div>
 
           <div className="chat-input">
-            <input type="text" name="message" value="" placeholder="type message here" id="chat-message"></input>
-            <input type="button" name="submit" value="send" id="send-message"></input>
+            <input type="text" name="message" value={this.state.newMessage} placeholder="type message here"></input>
+            <input type="button" onClick={this.submitMessage}></input>
           </div>
 
         </div>
@@ -24,5 +60,7 @@ var ChatModule = React.createClass({
     );
   }
 });
+
+
 
 module.exports = ChatModule;
